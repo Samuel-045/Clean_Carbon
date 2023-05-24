36c17b9ff0c0e127e9f63db128daa5befe9218e7
Samuel Victor <95144250+Samuel-045@users.noreply.github.com>
2023-05-24 10:42:49 -0300

    Chamada da mensagem de "Bem vindo"

--- Cod_PI.java
+++ Cod_PI.java
@@ -3,6 +3,7 @@ package projeto_PI;
 public class Cod_PI {
 	public static void main(String args[]) {
 		
+		MensagemBv.Mensg();
 		PartColt.partColt();
 		PartInd.partIndv(PartColt.fml, PartColt.carbonCol, PartColt.nResci);
 	}
