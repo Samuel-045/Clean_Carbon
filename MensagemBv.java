401e06691607e0843315d5918df22980cbb8c9e8
Samuel Victor <95144250+Samuel-045@users.noreply.github.com>
2023-05-24 10:43:25 -0300

    Create MensagemBv.java

---
+++ MensagemBv.java
@@ -0,0 +1,27 @@
+package projeto_PI;
+
+public class MensagemBv {
+
+public static void Mensg() {
+	
+		System.out.println("-------------------------------------------------------------");
+		System.out.println(      "  #####    ##        ######     ##     ##    ##        ");
+		System.out.println(      "##     ##  ##        ##        ####    ###   ##        ");
+		System.out.println(      "#          ##        ##       ##  ##   ####  ##        ");
+		System.out.println(      "#          ##        ######  ##    ##  ## ## ##        ");
+		System.out.println(      "#          ##        ##      ########  ##  ####        ");
+		System.out.println(      "##     ##  ##        ##      ##    ##  ##    ##        ");
+		System.out.println(      "  #####    ########  ######  ##    ##  ##    ##        ");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("  #####       ##     ######    ######      #####     ##    ##");
+		System.out.println("##     ##    ####    ##    ##  ##    ##   ##    ##   ###   ##");
+		System.out.println("#           ##  ##   ##    ##  ##    ##  ##      ##  ####  ##");
+		System.out.println("#          ##    ##  ##    ##  ######    ##      ##  ## ## ##");
+		System.out.println("#          ########  ######    ##    ##  ##      ##  ##  ####");
+		System.out.println("##     ##  ##    ##  ##   ##   ##    ##   ##    ##   ##    ##");
+		System.out.println("  #####    ##    ##  ##    ##  ######      #####     ##    ##");
+		System.out.println("-------------------------------------------------------------");
+	
+}
+	
+}
